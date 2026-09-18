3a0e2c703bf601240529f163626edfeaddd4d68c
qamaratwork00-cmyk <qamaratwork00@gmail.com>
2026-09-18 17:43:17 +0500

    Phase 8: export management execution bridge

--- src/goldswingtraderai/management/__init__.py
+++ src/goldswingtraderai/management/__init__.py
@@ -1,5 +1,9 @@
 """Post-entry Trade Manager public API."""
 
+from goldswingtraderai.management.execution import (
+    apply_verified_management_result,
+    management_intent_from_decision,
+)
 from goldswingtraderai.management.manager import (
     TradeManagerConfig,
     derive_management_evidence,
@@ -25,7 +29,9 @@ __all__ = [
     "TradeManagementDecision",
     "TradeManagerConfig",
     "apply_management_decision",
+    "apply_verified_management_result",
     "derive_management_evidence",
     "evaluate_trade_manager",
     "managed_trade_from_fill",
+    "management_intent_from_decision",
 ]
