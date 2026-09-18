030ad4bb81b45fe581a1dee7ba8a4a9445ced535
qamaratwork00-cmyk <qamaratwork00@gmail.com>
2026-09-18 22:44:25 +0500

    Security: add shared financial-secret scanner package

---
+++ src/goldswingtraderai/security/__init__.py
@@ -0,0 +1,5 @@
+"""Security helpers with no trading authority."""
+
+from goldswingtraderai.security.financial_secrets import scan_text_for_financial_secrets
+
+__all__ = ["scan_text_for_financial_secrets"]
